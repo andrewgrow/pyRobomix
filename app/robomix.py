1b8ea22aecd933cdb9ef9dcdd63baff201390770
Andrew Gahov <andrew.gahov@gmail.com>
2018-05-28 21:18:12 +0300

    add a test response

--- app/robomix.py
+++ app/robomix.py
@@ -3,10 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def hello():
     return "<h1 style='color:blue'>Hello There 4!</h1>"
 
 
+@app.route('/v1', methods=['POST'])
+def api_request() -> str:
+    return "Test response"
+
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1')
+    app.run(host='127.0.0.1', debug=True)
