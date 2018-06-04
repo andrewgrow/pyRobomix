cd53eba3c29d992a20e0a0f7345d6f449041d00b
Andrew Gahov <andrew.gahov@gmail.com>
2018-06-04 22:04:13 +0300

    add robots and sitemap

--- robomix/robomix.py
+++ robomix/robomix.py
@@ -77,5 +77,11 @@ def api_request() -> str:
     return "Test response"
 
 
+@app.route('/sitemap.xml')
+@app.route('/robots.txt')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
+
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True)
